3f1e345649dbe051d815560ad888391acace6747
Juancito2085 <jcbrunello@hotmail.com>
2024-12-19 23:30:54 -0300

    scrapear cada propiedad

--- inmuebles/inmuebles/spiders/alquileres_rosario.py
+++ inmuebles/inmuebles/spiders/alquileres_rosario.py
@@ -12,25 +12,21 @@ class AlquileresRosarioSpider(scrapy.Spider):
 
         inmuebles = response.css(".card")
         for inmueble in inmuebles:
-            expenses_text=inmueble.css(".card__expenses::text").get()
-            if expenses_text != None:
-                expensas = expenses_text.split(';')[1].replace('\nexpensas','').strip()
-                expensas = expensas.replace('"','')
-            else:
-                expensas = 0
-
-            yield {
-                "precio": inmueble.css('.card__price').get().split(' ')[27].replace('\n', ''),
-                "moneda": inmueble.css(".card__currency::text").get(),
-                "expensas": expensas,
-                "ubicación": inmueble.css('p.card__address::text').get().replace('\n','').strip(),
-                "url": inmueble.css("a::attr(href)").get(),
-            }
-        pagina_siguiente = response.css('[rel="next"]::attr(href)').get()
+            url_relativa = inmueble.css("a::attr(href)").get()
+            url_inmueble = 'https://www.argenprop.com' + url_relativa
+            print(url_inmueble)
+            yield response.follow(url_inmueble, callback=self.pag_inmueble_parse)
+#        pagina_siguiente = response.css('[rel="next"]::attr(href)').get()
         
-        if pagina_siguiente:
+ #       if pagina_siguiente:
             
-            url_pagina_siguiente = "https://www.argenprop.com" + pagina_siguiente
-            print(url_pagina_siguiente)
-            yield response.follow(url_pagina_siguiente, callback=self.parse)
+  #          url_pagina_siguiente = "https://www.argenprop.com" + pagina_siguiente
+   #         print(url_pagina_siguiente)
+    #        yield response.follow(url_pagina_siguiente, callback=self.parse)
 
+    def pag_inmueble_parse(self, response):
+        
+        yield {
+            'precio':response.css('p.titlebar__price::text').get().replace('\n','').strip(),
+
+        }

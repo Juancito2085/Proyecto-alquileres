d001218abdbf61cb904d7a86204ab266ca7f54b3
Juancito2085 <jcbrunello@hotmail.com>
2024-12-23 13:34:17 -0300

    Listo para pruebas

--- inmuebles/inmuebles/spiders/alquileres_rosario.py
+++ inmuebles/inmuebles/spiders/alquileres_rosario.py
@@ -30,19 +30,75 @@ class AlquileresRosarioSpider(scrapy.Spider):
             expensas = texto_expensas.split()[1]
         else:
             expensas = 'No tiene'
+        
+        ambientes = response.css('li[title="Ambientes"] p::text').get()
+        if ambientes:
+            ambientes = ambientes.split()[0]
+        else:
+            ambientes = 'Sin datos'
+        
+        dormitorios = response.css('li[title="Dormitorios"] p::text').get()
+        if dormitorios:
+            dormitorios = dormitorios.split()[0]
+        else:
+            dormitorios = 'Sin datos'
+        
+        baños = response.css('li[title="Baños"] p::text').get()
+        if baños:
+            baños = baños.split()[0]
+        else:
+            baños = 'Sin datos'
+
+        superficie_cubierta = response.css('li[title="Sup. cubierta"] p::text').get()
+        if superficie_cubierta:
+            superficie_cubierta = superficie_cubierta.split()[0]
+        else:
+            superficie_cubierta = 'Sin datos'
+
+        antiguedad = response.css('li[title="Antiguedad"] p::text').get()
+        if antiguedad:
+            antiguedad = antiguedad
+        else:
+            antiguedad = 'Sin datos'
+
+        orientacion = response.css('li[title="Orientación"] p::text').get()
+        if orientacion:
+            orientacion = orientacion
+        else:
+            orientacion = 'Sin datos'
+
+        estado = response.css('li[title="Estado"] p::text').get()
+        if estado:
+            estado = estado
+        else:
+            estado = 'Sin datos'
+
+        disposicion = response.css('li[title="Disposición"] p::text').get()
+        if disposicion:
+            disposicion = disposicion
+        else:
+            disposicion = 'Sin datos'
+
+        tipo = response.css('ul.property-main-features li:first-child p::text').get()
+        if tipo:
+            tipo = tipo
+        else:  
+            tipo = 'Sin datos'
+
         yield {
             'precio':response.css('p.titlebar__price::text').get().replace('\n','').strip(),
             'expensas':expensas,
             'direccion':response.css('h2.titlebar__address::text').get(),
             'latitud':response.css('[data-latitude]::attr(data-latitude)').get(),
             'longitud':response.css('[data-longitude]::attr(data-longitude)').get(),
-            'ambientes':response.css('li[title="Ambientes"] p::text').get().split()[0],
-            'dormitorios':response.css('li[title="Dormitorios"] p::text').get().split()[0],
-            'baños':response.css('li[title="Baños"] p::text').get().split()[0],
-            'superficie cubierta':response.css('li[title="Sup. cubierta"] p::text').get().split()[0],
-            'antiguedad':response.css('li[title="Antiguedad"] p::text').get(),
-            'orientacion':response.css('li[title="Orientación"] p::text').get(),
-            'estado':response.css('li[title="Estado"] p::text').get(),
-            'disposicion':response.css('li[title="Disposición"] p::text').get(),
-            'tipo':response.css('ul.property-main-features li:first-child p::text').get()
+            'ambientes':ambientes,
+            'dormitorios':dormitorios,
+            'baños':baños,
+            'superficie cubierta':superficie_cubierta,
+            'antiguedad':antiguedad,
+            'orientacion':orientacion,
+            'estado':estado,
+            'disposicion':disposicion,
+            'tipo':tipo
+            
         }
